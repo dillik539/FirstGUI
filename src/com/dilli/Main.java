14c88a00394922ae5e9a44f5d6528967ae5dcb5a
Dilli <dilli@dilli.com>
2017-03-22 19:27:44 -0500

    FirstGUI program

---
+++ src/com/dilli/Main.java
@@ -0,0 +1,9 @@
+package com.dilli;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        PetSurvey survey = new PetSurvey();
+    }
+}
---
+++ src/com/dilli/PetSurvey.java
@@ -0,0 +1,81 @@
+package com.dilli;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ * Created by dilli on 3/22/2017.
+ */
+public class PetSurvey extends JFrame{
+    private JPanel rootPanel;
+    private JCheckBox dogCheckbox;
+    private JCheckBox fishCheckBox;
+    private JCheckBox catCheckBox;
+    private JButton quitButton;
+    private JLabel resultsLabel;
+
+    private boolean dog;
+    private boolean cat;
+    private boolean fish;
+
+    protected PetSurvey(){
+        setContentPane(rootPanel);
+        pack();
+        setSize(new Dimension(350,200));
+        setTitle("Pet Survey");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        fishCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                fish = fishCheckBox.isSelected();
+                updateResults();
+            }
+        });
+        catCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                cat = catCheckBox.isSelected();
+                updateResults();
+            }
+        });
+        dogCheckbox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                dog = dogCheckbox.isSelected();
+                updateResults();
+            }
+        });
+        quitButton.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+
+            }
+        });
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int quit = JOptionPane.showConfirmDialog(PetSurvey.this,"Are you sure you want to quit?",
+                        "Quit",JOptionPane.OK_CANCEL_OPTION);
+                if(quit == JOptionPane.OK_OPTION){
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+    private void updateResults() {
+        String hasDog = dog ? "one dog" : "no dogs";
+        String hasCat = cat ? "one cat" : "no cats";
+        String hasFish = fish ? "one fish" : "no fish";
+
+        String results = "You have " + hasDog + " and " + hasCat + " and " + hasFish;
+        resultsLabel.setText(results);
+    }
+}
